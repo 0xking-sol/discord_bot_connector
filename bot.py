a4cd7c222d3ae45d2da6248cd10c6203d8665d00
0xking-sol <124360861+0xking-sol@users.noreply.github.com>
2023-02-13 15:51:47 +0000

    Update bot.py
    Added in functionality to detect whether the coin is listed on binance using an index. Removed some errors that would come out

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import random
 import responses
-import CEXs
+from CEXs import currency_list
 
 
 async def send_message(message, user_message, is_private):
@@ -13,7 +13,6 @@ async def send_message(message, user_message, is_private):
 
 
 # do we want to send in channel or as private message
-
 
 def run_discord_bot():
     TOKEN = 'MTA3Mjg5NTQwMTE2Mzc3MTk3Ng.GZ7fyG.aduVf2J1YVIwn9OXotfzp7uPjx1C2A6fOt3t3g'
@@ -34,44 +33,42 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f'{username} said: "{user_message}" ({channel})')
-
         if len(message.embeds) == 1:
             embed = message.embeds[0]
             embed.title = embed.title.lower()
             embed.description = embed.description.lower()
+            message_currency = embed.title.split(" on ")[-1].split("\n")[0]
+            message_currency = message_currency.upper()
+            print(f'{username} said: "{embed.title}" ({channel})')
+
+            if message_currency in currency_list:
+                index = currency_list.index(message_currency)
+            else:
+                index = 0
+                currency_list[index] = '- coin not on binance'
+
             if "twap" in embed.title:
                 if "buying" in embed.description:
-                    #if currency in embed.title:
-                    await message.channel.send('twap buy activated, opening a long on ' + currency)
+                    await message.channel.send('twap buy activated, opening a long on ' + currency_list[index])
                 if "selling" in embed.description:
-                    #if currency in embed.title:
-                    await message.channel.send('twap sell activated, opening a short on ' + currency)
+                    await message.channel.send('twap sell activated, opening a short on ' + currency_list[index])
 
             if "market sell" in embed.title:
-                if currency in embed.title:
-                    await message.channel.send('market sold, opening a long on ' + currency)
+                await message.channel.send('market sold, opening a long on ' + currency_list[index])
 
             if "market buy" in embed.title:
-                if currency in embed.title:
-                    await message.channel.send('market bought, opening a short on ' + currency)
+                await message.channel.send('market bought, opening a short on ' + currency_list[index])
 
             if "test notification" in embed.title:
                 await message.channel.send('test doubly approved')
 
-            # this won't work until i split the dictionary into a string - maybe it will work now?
-            #for currency in currencies:
-            #currency = str(currency)
-             #   if currency in embed.title:
-              #      await message.channel.send('relevant currency is ' + currency)
-
-        if user_message[0] not in range:
-            return
-        else:
-            if user_message[0] == '?':
+            if len(user_message) > 0 and user_message[0] == '?':
                 user_message = user_message[1:]
                 await send_message(message, user_message, is_private=True)
             else:
                 await send_message(message, user_message, is_private=False)
+
+        else:
+            print(f'{username} said: "{user_message}" ({channel})')
 
     client.run(TOKEN)
